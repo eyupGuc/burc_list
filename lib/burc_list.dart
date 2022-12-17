bf7ef6071e6e5e2fae000fffc9b781f8919fb7ff
eyupGuc <meydanguresi@gmail.com>
2022-12-17 04:38:59 +0300

    data

--- lib/burc_list.dart
+++ lib/burc_list.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/material.dart';
 
+import 'modal/burc.dart';
+
 class BurcList extends StatelessWidget {
-  const BurcList({Key? key}) : super(key: key);
+  late List<Burc> tumBurclar;
+  BurcList() {
+    tumBurclar = veriKaynaginiHazirla();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Burçlar listesi'),),body: Center(child: Text('Burç listesi buraya gelecek'),),);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Burçlar listesi'),
+      ),
+      body: Center(
+        child: Text('Burç listesi buraya gelecek'),
+      ),
+    );
+  }
+
+  List<Burc> veriKaynaginiHazirla() {
+    List<Burc> gecici=[];
+    return gecici;
   }
 }
---
+++ lib/modal/burc.dart
@@ -0,0 +1,9 @@
+class Burc{
+  final String _burcAdi;
+  final String _burcTarihi;
+  final String _burcDetayi;
+  final String _burcKucukResim;
+  final String _burcBuyukResin;
+
+  Burc(this._burcAdi, this._burcTarihi, this._burcDetayi, this._burcKucukResim, this._burcBuyukResin);
+}
