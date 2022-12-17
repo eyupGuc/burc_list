63edc5ca358d5cbbf3e8a884d5be1ad47f5d36d1
eyupGuc <meydanguresi@gmail.com>
2022-12-17 04:08:15 +0300

    first commit

---
+++ lib/burc_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BurcList extends StatelessWidget {
+  const BurcList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(child: Text("list"),);
+  }
+}
